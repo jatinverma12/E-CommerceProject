59525aec51176a0f5e17d91814a1ef88013cfd11
jatinverma4621 <44510976+jatinverma12@users.noreply.github.com>
2020-07-25 01:01:10 +0530

    done

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.use(productsRouter);
 app.use(adminProductsRouter);
 app.use(cartsRouter);
 
-app.listen(3000 || process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Listening');
 });
